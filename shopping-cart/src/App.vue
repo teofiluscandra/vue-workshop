3bc7ea98bd252e36a753b93c8afdbeff84ee92cc
Teofilus Candra <teofiluschandra@gmail.com>
2018-07-25 17:35:54 +0800

    Learn about component and add navbar

---
+++ shopping-cart/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <NavBar />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
